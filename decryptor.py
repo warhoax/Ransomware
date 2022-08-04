ab93f1695e000703e69d08e9e1447c645cfc87ee
warhoax <iwarhoax@gmail.com>
2022-08-04 04:11:45 -0700

    1

--- decryptor.py
+++ decryptor.py
@@ -1,1 +1,14 @@
-print('decryptor')
+from cryptography.fernet import Fernet
+
+with open("key.key", "rb") as kf:
+    key = kf.read()
+
+fernet = Fernet(key)
+
+with open("test.txt","rb") as tf:
+    tf_bytes = tf.read()
+
+    tf_bytes_dec = fernet.decrypt(tf_bytes)
+
+with open("test.txt","wb") as tf:
+    tf.write(tf_bytes_dec)
--- encryptor.py
+++ encryptor.py
@@ -1,1 +1,17 @@
-print('encryptor')
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+
+with open("key.key", "wb") as keyFile:
+    keyFile.write(key)
+
+fernet = Fernet(key)
+
+with open("test.txt", "rb") as tf:
+    tf_byte = tf.read()
+
+tf_bytes_enc = fernet.encrypt(tf_byte)
+
+with open("test.txt", "wb") as tf:
+     tf.write(tf_bytes_enc)
+
